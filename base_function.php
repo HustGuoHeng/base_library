c4bc2ed3b1118ab5d15b54b8126909b1ae3ce67e
guoheng <guoheng@qiyi.com>
2016-11-10 19:57:58 +0800

    update

--- base_function.php
+++ base_function.php
@@ -21,6 +21,9 @@ function curlRequest($url, $params = array(), $isPost = false, $timeOut = 60) {
         $result = curl_exec($ch);
         curl_close($ch);
     } else {
+        if ($params) {
+            $url .= http_build_query($params);
+        }
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_HEADER, 0);
